b2466a8f886812fc867e4cbd267135d95eecb174
RiLoGosh <simon.william.hocker@gmail.com>
2025-06-13 02:33:56 +0200

    fix: made it so that food category loads in flex row while the rest of the filter type buttons load in flex col

--- restaurant-app/app/components/sidebarFilter.jsx
+++ restaurant-app/app/components/sidebarFilter.jsx
@@ -3,23 +3,28 @@ import FilterButton from './filterButton'
 
 export default function FilterSidebar({ filterMap, filterTypes }) {
   
-  let i = 0;
+
     return (
-    <div className="flex flex-col border-[0.6px] border-umainstroke bg-umainwhite rounded-[10px] w-[239px] h-[764px]">
-        <h2 className="text-lg font-semibold w-[49px] h-[24px]-">Filter</h2>
+        
 
-        {filterTypes.map((filter, index) => (
-            <div key={index} className="flex flex-col w-auto h-auto mb-2">
-                <h1>{filter}</h1>
-                {filterMap.get(filter).map((category, i) => (
-                    <FilterButton key={i} filterOption={category} className=''/>
-
+        <div className="flex flex-col border-[0.6px] border-umainstroke bg-umainwhite rounded-[10px] w-[239px] h-[764px]">
+            <h2 className="text-lg font-semibold w-[49px] h-[24px]">Filter</h2>
+            <div>
+                {filterTypes.map((filter, index) => (
+                    <div
+                        key={index}
+                        className={`${
+                            index === 0 ? 'flex flex-col' : 'flex flex-row flex-wrap'
+                        } w-auto h-auto mb-2`}
+                    >
+                        <h1 className="w-full">{filter}</h1>
+                        {filterMap.get(filter).map((category, i) => (
+                            <FilterButton key={i} filterOption={category} className="" />
+                        ))}
+                    </div>
                 ))}
-
-                
             </div>
-        ))}
-      
-    </div>
+        </div>
+
   );
 }
