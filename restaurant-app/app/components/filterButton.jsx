fc9695ba539e51ba68535ad82f347f4639215fb2
RiLoGosh <simon.william.hocker@gmail.com>
2025-06-13 10:43:52 +0200

    fix: added some CSS styling to the filter sidebar, mainly spacing

--- restaurant-app/app/components/filterButton.jsx
+++ restaurant-app/app/components/filterButton.jsx
@@ -4,8 +4,8 @@ export default function FilterButton({ filterOption }){
 
 
     return (
-        <div className="w-auto h-auto rounded-[8px] py-[8px] px-[12px] gap-[8px] border-solid border-umainstroke border-[0.6px]">
-            <button>
+        <div className="w-auto h-auto rounded-[8px] py-[8px] px-[12px]  border-solid border-umainstroke border-[0.6px]">
+            <button className="text-[12px]">
                 {filterOption}
             </button>
         </div>
--- restaurant-app/app/components/sidebarFilter.jsx
+++ restaurant-app/app/components/sidebarFilter.jsx
@@ -7,16 +7,16 @@ export default function FilterSidebar({ filterMap, filterTypes }) {
     return (
         
         <div className="flex flex-col border-[0.6px] border-umainstroke bg-umainwhite rounded-[10px] w-[239px] h-[764px]">
-            <h2 className="text-lg font-semibold w-[49px] h-[24px]">Filter</h2>
-            <div>
+            <h2 className="text-lg text-[24px] p-4 font-semibold w-[49px] h-[24px]">Filter</h2>
+            <div className='p-4 gap-5'>
                 {filterTypes.map((filter, index) => (
                     <div
                         key={index}
                         className={`${
                             index === 0 ? 'flex flex-col gap-[10px]' : 'flex flex-row flex-wrap gap-[16px]'
-                        } w-auto h-auto mb-2`}
+                        } w-auto h-auto mb-2 p-4`}
                     >
-                        <h1 className="w-full">{filter}</h1>
+                        <h1 className="w-[98px] h-[12px] opacity-40 text-[12px] uppercase">{filter}</h1>
                         {filterMap.get(filter).map((category, i) => (
                             <FilterButton key={i} filterOption={category} className="" />
                         ))}
